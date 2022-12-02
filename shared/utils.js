2ae09cc6cd48f6b5d6f27984c01511a5a2c1f4ab
Noor Alizah <4340264-nooralizahar@users.noreply.gitlab.com>
2022-12-02 07:58:35 +0700

    update

--- shared/utils.js
+++ shared/utils.js
@@ -32,7 +32,7 @@ class Reticle extends THREE.Object3D {
   }
 }
 
-window.gltfLoader.load("/assets/logo_bj.glb", function(gltf) {
+window.gltfLoader.load("../assets/logo_bj.glb", function(gltf) {
   const flower = gltf.scene.children.find(c => c.name === 'logo_bj')
   flower.castShadow = true;
   window.sunflower = gltf.scene;
