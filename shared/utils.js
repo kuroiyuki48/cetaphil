453e0510616c502dcb87a3fe574066aaeae68cbf
Noor Alizah <4340264-nooralizahar@users.noreply.gitlab.com>
2022-12-08 20:06:51 +0700

    update

--- shared/utils.js
+++ shared/utils.js
@@ -22,102 +22,34 @@ window.gltfLoader = new THREE.GLTFLoader();
 class Reticle extends THREE.Object3D {
   constructor() {
     super();
+    var loader = new THREE.GLTFLoader();
+      // Load a glTF resource
+      loader.load(
+        // resource URL
+        'https://kuroiyuki48.github.io/cetaphil/assets/bj_logo_5.glb',
+        // called when the resource is loaded
+        function ( gltf ) {
 
-    this.loader = new THREE.GLTFLoader();
-    this.loader.load("https://kuroiyuki48.github.io/cetaphil/assets/bj_logo_5.glb", (gltf) => {
-      this.add(gltf.scene);
-    })
+          gltf.scene.position.setFromMatrixPosition( reticle.matrix );
+          model = gltf;
+          scene.add( model.scene );
 
-    this.visible = false;
+          model.scene.addEventListener( "touchstart", handleTouchStart, false );
+          model.scene.addEventListener( "touchend", handleTouchEnd, false );
+          model.scene.addEventListener( "touchmove", handleTouchMove, false );
+      }
+    );
+    loader();
+
+    // this.loader = new THREE.GLTFLoader();
+    // this.loader.load("https://kuroiyuki48.github.io/cetaphil/assets/bj_logo_5.glb", (gltf) => {
+    //   this.add(gltf.scene);
+    // })
+
+    // this.visible = false;
   }
 }
 
-// window.gltfLoader.load("https://kuroiyuki48.github.io/cetaphil/assets/bj_logo_3.glb", function(gltf) {
-//   window.logo_bj_2 = gltf.scene;
-// });
-
-
-window.DemoUtils = {
-  /**
-   * Creates a THREE.Scene containing lights that case shadows,
-   * and a mesh that will receive shadows.
-   *
-   * @return {THREE.Scene}
-   */
-  createLitScene() {
-    const scene = new THREE.Scene();
-
-    // The materials will render as a black mesh
-    // without lights in our scenes. Let's add an ambient light
-    // so our material can be visible, as well as a directional light
-    // for the shadow.
-    const light = new THREE.AmbientLight(0xffffff, 1);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-    directionalLight.position.set(10, 15, 10);
-
-    // We want this light to cast shadow.
-    directionalLight.castShadow = true;
-
-    // Make a large plane to receive our shadows
-    const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
-    // Rotate our plane to be parallel to the floor
-    planeGeometry.rotateX(-Math.PI / 2);
-
-    // Create a mesh with a shadow material, resulting in a mesh
-    // that only renders shadows once we flip the `receiveShadow` property.
-    const shadowMesh = new THREE.Mesh(planeGeometry, new THREE.ShadowMaterial({
-      color: 0x111111,
-      opacity: 0.2,
-    }));
-
-    // Give it a name so we can reference it later, and set `receiveShadow`
-    // to true so that it can render our model's shadow.
-    shadowMesh.name = 'shadowMesh';
-    shadowMesh.receiveShadow = true;
-    shadowMesh.position.y = 10000;
-
-    // Add lights and shadow material to scene.
-    scene.add(shadowMesh);
-    scene.add(light);
-    scene.add(directionalLight);
-
-    return scene;
-  },
-
-  /**
-   * Creates a THREE.Scene containing cubes all over the scene.
-   *
-   * @return {THREE.Scene}
-   */
-  createCubeScene() {
-    const scene = new THREE.Scene();
-
-    const materials = [
-      new THREE.MeshBasicMaterial({ color: 0xff0000 }),
-      new THREE.MeshBasicMaterial({ color: 0x0000ff }),
-      new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
-      new THREE.MeshBasicMaterial({ color: 0xff00ff }),
-      new THREE.MeshBasicMaterial({ color: 0x00ffff }),
-      new THREE.MeshBasicMaterial({ color: 0xffff00 })
-    ];
-
-    const ROW_COUNT = 4;
-    const SPREAD = 1;
-    const HALF = ROW_COUNT / 2;
-    for (let i = 0; i < ROW_COUNT; i++) {
-      for (let j = 0; j < ROW_COUNT; j++) {
-        for (let k = 0; k < ROW_COUNT; k++) {
-          const box = new THREE.Mesh(new THREE.BoxBufferGeometry(0.2, 0.2, 0.2), materials);
-          box.position.set(i - HALF, j - HALF, k - HALF);
-          box.position.multiplyScalar(SPREAD);
-          scene.add(box);
-        }
-      }
-    }
-
-    return scene;
-  },
-};
 
 /**
  * Toggle on a class on the page to disable the "Enter AR"
